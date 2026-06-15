bf425c529f9f296554b698a66bf215a79e944593
hisuilab <tech@hisuilab.com>
2026-06-15 23:11:16 +0900

    fix(p10k):update gruvbox theme color settings

--- modules/home/p10k/conf.d/theme/gruvbox.zsh
+++ modules/home/p10k/conf.d/theme/gruvbox.zsh
@@ -1,25 +1,26 @@
 # ============================================================
 # Gruvbox Theme Palette
 # ============================================================
+https://github.com/morhetz/gruvbox
 
 # Base
-local -r C_DARK=235      # #262626 dark bg
-local -r C_WHITE=223     # #FFD7AF warm cream fg
-local -r C_BLACK=234     # #1C1C1C harder bg
+local -r C_DARK=235
+local -r C_WHITE=223
+local -r C_BLACK=0
 
 # Primary
-local -r C_PRIMARY=66    # #5F8787 muted teal (dir)
-local -r C_ACCENT=95     # #875F5F muted mauve (vcs modified)
+local -r C_PRIMARY=142
+local -r C_ACCENT=214
 
 # Semantic
-local -r C_SUCCESS=108   # #87AF87 muted green
-local -r C_SUCCESS_ALT=142  # #AFAF5F yellow-green
-local -r C_WARNING=136   # #AF8700 amber
-local -r C_ERROR=167     # #D75F5F warm red
-local -r C_LANG_WARM=167 # #D75F5F warm red (Ruby/Scala/Erlang/Laravel)
+local -r C_SUCCESS=108
+local -r C_SUCCESS_ALT=109
+local -r C_WARNING=208
+local -r C_ERROR=167
+local -r C_LANG_WARM=166
 
 # VCS foreground (on top of semantic background colors)
-typeset -g C_VCS_CLEAN_FG=234
-typeset -g C_VCS_MODIFIED_FG=223
-typeset -g C_VCS_UNTRACKED_FG=136
-typeset -g C_VCS_CONFLICTED_FG=167
+typeset -g C_VCS_CLEAN_FG=0
+typeset -g C_VCS_MODIFIED_FG=255
+typeset -g C_VCS_UNTRACKED_FG=255
+typeset -g C_VCS_CONFLICTED_FG=255
